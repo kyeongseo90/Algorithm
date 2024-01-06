5dacde0161b9e4512f46f23ffcee3ae42a16c9ea
kyeongseo90 <59429729+kyeongseo90@users.noreply.github.com>
2024-01-06 11:31:09 +0900

    [Gold V] Title: 강의실 배정, Time: 536 ms, Memory: 148584 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,16 @@
+import sys, heapq
+n = int(sys.stdin.readline())
+clss = []
+for _ in range(n):
+    clss.append(list(map(int, input().split())))
+clss.sort()
+room = []
+heapq.heappush(room, clss[0][1]) # 최소힙 작은순서대로 정렬
+
+for i in range(1, n):
+    if clss[i][0] < room[0]:
+        heapq.heappush(room, clss[i][1]) # 새로운 강의의 끝나는 시간만 넣기
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, clss[i][1]) # 대체된 강의의 끝나는 시간만 넣기
+print(len(room))
