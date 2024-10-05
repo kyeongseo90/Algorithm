a1b4c8b1000f5a19d589975c89e05fabb3e1b4ed
kyeongseo90 <rudtj9099@gmail.com>
2024-10-05 21:50:18 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,11 +1,13 @@
 def solution(n):
-    ans = 0
-    
-    while n != 0:
-        if n % 2 == 0:
-            n /= 2
-        else:
-            n -= 1
-            ans += 1
+    # 방법 2
+    return bin(n).count('1');
 
-    return ans
+    # # 방법 1
+    # ans = 0
+    # while n != 0:
+    #     if n % 2 == 0:
+    #         n /= 2
+    #     else:
+    #         n -= 1
+    #         ans += 1
+    # return ans
