2db8d1122600c2a9fd55956d33935ceb459b3622
kyeongseo90 <rudtj9099@gmail.com>
2024-10-26 13:43:23 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,6 @@
-SELECT animal_id, name
-from animal_outs
-minus
-select animal_id, name
-from animal_ins
+SELECT O.animal_id, O.name
+from animal_outs as O
+where O.animal_id not in (
+    select I.animal_id
+    from animal_ins AS I
+)
