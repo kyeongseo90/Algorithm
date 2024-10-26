343c906b0367d771e4be537c3880b0ee074885c9
kyeongseo90 <rudtj9099@gmail.com>
2024-10-26 14:18:38 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -11,19 +11,3 @@ join ( # 부모가 있고, 그 부모가 rare한 애들
     where E.parent_item_id is not null # 그리고 parent_item이 rare이어야 함
 ) as T on I.item_id = T.item_id
 order by I.item_id desc
-
-# select I.item_id, I.item_name, I.rarity
-# from item_tree as T
-# join (
-#     select item_id, item_name, rarity
-#     from item_info
-#     where rarity = "RARE"
-# ) as I on T.parent_item_id = I.item_id
-# where T.parent_item_id is not null
-# order by I.item_id desc
-
-# select item_id, item_name, rarity
-# from item_info
-# where rarity = "RARE"
-# # left join 
-# order by item_id desc
